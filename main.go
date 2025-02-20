fa6fa0b8efcbb09c34edadbbd26c7b49146db621
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-02-20 15:42:47 +0530

    feat: add route to get a single event

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/yogeshbhutkar/go-jwt-with-db-template/db"
@@ -15,6 +16,7 @@ func main() {
 	server.GET("/", healthCheck)
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", postEvents)
 
 	server.Run(":8080")
@@ -32,6 +34,31 @@ func getEvents(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusOK, gin.H{"events": events})
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(
+			http.StatusBadRequest,
+			gin.H{"message": "could not parse event id"},
+		)
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(
+			http.StatusInternalServerError,
+			gin.H{"message": "Could not fetch event"},
+		)
+		return
+	}
+
+	context.JSON(
+		http.StatusOK,
+		gin.H{"event": event},
+	)
 }
 
 func postEvents(context *gin.Context) {
